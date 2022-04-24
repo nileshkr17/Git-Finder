d7da937375f4cbf1a797ea9ce4dafe1c20d0fdd9
Nilesh <ece.20becd76@silicon.ac.in>
2022-04-25 00:42:09 +0530

    [js]

--- index.js
+++ index.js
@@ -7,5 +7,39 @@ getUser("nileshkr17");
 
 async function getUser(username)
 {
+    const resp = await fetch(APIURL + username);
+    const respData = await resp.json();
+    createUserCard(respData);
+    getRepos(username);
+
+
+}
+async function getElementById(username)
+{
+    const resp = await fetch(APIURL + username + "/repos");
+    const resphData = await resp.json();
+     
+    addReposToCard(respData);
+
+}
+function createUserCard(user)
+{
+    const cardHTML = 
+    `<div class = "card">
+    <div>
+        <img class="avatar" src ="${user.avatar_url}" alt="${user.name}"
+        </div>
+        <div class="user-info">
+            <h2>${user.name}</h2>
+            <p>${user.bio}</p>
+            <ul class="info">
+            <li>${user.followers}<strong>Followers</strong></li>
+            <li>${user.following}<strong>Following</strong></li>
+            <li>${user.public_repos}<strong>Repos</strong></li>
+        </ul>
+        <div id="repos"></div>
+    </div>
+</div>
+`;
     
 }
